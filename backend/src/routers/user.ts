87bb4ff0d93b5aa6d6c89324f7ff0b54e73eae26
Gosh95 <goshk1995@gmail.com>
2022-12-09 13:30:32 +0900

    feat: 유저 라우터 인증 미들웨어 추가

--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -3,27 +3,45 @@ import { Router } from 'express';
 import { Routers } from '../types/layers';
 import UserValidator from '../middlewares/validators/user';
 import UserController from '../controllers/user/user';
+import JwtAuth from '../middlewares/auth/jwt';
 
 class UserRouter implements Routers {
   public path;
   public router;
   private userValidator;
   private userController;
+  private jwtAuth;
 
   constructor() {
     this.path = '/api/users';
     this.router = Router();
     this.userValidator = new UserValidator();
     this.userController = new UserController();
+    this.jwtAuth = new JwtAuth();
 
     this.initRouter();
   }
 
   initRouter() {
     this.router.post('/', this.userValidator.validateCreateUser(), this.userController.createUser());
-    this.router.get('/:userId', this.userValidator.validateUserIdParam(), this.userController.getUserDetail());
-    this.router.patch('/:userId', this.userValidator.validateUpdateUser(), this.userController.updateUser());
-    this.router.delete('/:userId', this.userValidator.validateDeleteUser(), this.userController.deleteUser());
+    this.router.get(
+      '/:userId',
+      this.userValidator.validateUserIdParam(),
+      this.jwtAuth.authenticate(),
+      this.userController.getUserDetail()
+    );
+    this.router.patch(
+      '/:userId',
+      this.userValidator.validateUpdateUser(),
+      this.jwtAuth.authenticate(),
+      this.userController.updateUser()
+    );
+    this.router.delete(
+      '/:userId',
+      this.userValidator.validateDeleteUser(),
+      this.jwtAuth.authenticate(),
+      this.userController.deleteUser()
+    );
   }
 }
 
